f33d0752b429b2063b2adeedff622d0005698537
Priw8 <priwe8@gmail.com>
2020-02-03 15:22:28 +0100

    add playerSetLives/Bombs

--- ECLplus/ins2100.cpp
+++ ECLplus/ins2100.cpp
@@ -14,6 +14,28 @@ BOOL ins_2100(ENEMY enm, INSTR* ins) {
         case INS_PLAYER_BOMB:
             ((void(__stdcall *)())0x00411C30)();
             break;
+        case INS_PLAYER_SET_LIVES:
+            PlayerLives = GetIntArg(enm, 0);
+            // look at code around th17.exe+49286 for reference
+            __asm {
+                mov ecx, DWORD_PTR[0x004B76AC] // this ptr
+                push DWORD_PTR[0x004B5A44]
+                push DWORD_PTR[PlayerLivesPtr] 
+                mov eax, 0x0042FC60
+                call eax
+            }
+            break;
+        case INS_PLAYER_SET_BOMBS:
+            PlayerBombs = GetIntArg(enm, 0);
+            // look at code around th17.exe+49286 for reference
+            __asm {
+                mov ecx, DWORD_PTR[0x004B76AC]
+                push DWORD_PTR[0x004B5A50]
+                push DWORD_PTR[PlayerBombsPtr]
+                mov eax, 0x0042FD50
+                call eax
+            }
+            break;
         default:
             return FALSE;
     }
--- ECLplus/ins2100.h
+++ ECLplus/ins2100.h
@@ -4,8 +4,16 @@
 enum INS2100 {
     INS_PLAYER_POS,
     INS_PLAYER_KILL,
-    INS_PLAYER_BOMB
+    INS_PLAYER_BOMB,
+    INS_PLAYER_SET_LIVES,
+    INS_PLAYER_SET_BOMBS
 };
+
+#define PlayerLivesPtr 0x004B5A40
+#define PlayerLives Deref(PlayerLivesPtr)
+
+#define PlayerBombsPtr 0x004B5A4C
+#define PlayerBombs Deref(PlayerBombsPtr)
 
 /* INS_2100 series: player manipulation */
 BOOL ins_2100(ENEMY enm, INSTR* ins);
